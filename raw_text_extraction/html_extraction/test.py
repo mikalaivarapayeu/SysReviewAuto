464b6bb90168ff7708ac1839e5780522757c4623
m_voropaev <voropaev.nv@gmail.com>
2023-08-21 18:02:35 +0300

    Scripts to constructed labelled chunks from MongoDB to use in fron-end applications

--- raw_text_extraction/html_extraction/test.py
+++ raw_text_extraction/html_extraction/test.py
@@ -8,84 +8,116 @@ db = client['clinicalTrialCorpus_v1']
 # print(coll_names)
 collection = db['method200SentencesMedicalArticles']
 
-sentences_list = list(collection.find())
 
 
-def find_sent(filter, updated):
-    update = { '$set': { "sentWords" : updated} }
-    filter = {'_id':filter}
-    collection.update_one(filter, update)
-    return
 
-
-sentence_label_set= [s['sentWords']for s in sentences_list]
-
-
-l2i = {i:k  for i, k in enumerate(list(set([wlp[1] for s in sentence_label_set for wlp in s])))}
-i2l = {k:i for i,k in l2i.items()}
-print(i2l)
 def sent_split_by_border_tag(word_label_list):
     sentences_split_by_border = []
     s_split =  []
     for s in word_label_list:
-        for wlp in s:
+        s_number = s[0]
+        for wlp in s[1]:
             if wlp[1] == 'BORDER':
-                s_split.append(wlp)
-                sentences_split_by_border.append(s_split)
+                # s_split.append(wlp)
+                sentences_split_by_border.append((s_number,s_split))
                 s_split = []
             else:
                 s_split.append(wlp)
-        sentences_split_by_border.append(s_split)
+        sentences_split_by_border.append((s_number,s_split))
         s_split = []
     return sentences_split_by_border
-
-sentences_split = sent_split_by_border_tag(sentence_label_set)
 
 def chunk_reconstruction(sentence):
     phrase_list = []
     chunked_phrase = []
     label = 'none'
-    for wlp in sentence:
+    s_number = sentence[0]
+    for wlp in sentence[1]:
         if wlp[1] not in ['none','ENDPHRASE'] and label == 'none':
             label = wlp[1]
             chunked_phrase.append(wlp[0])
         elif wlp[1] not in ['none', 'ENDPHRASE'] and label != 'none':
             # chunked_phrase.append(wlp[0])
-            phrase_list.append({wlp[1]: chunked_phrase[0]})
+            phrase_list.append((s_number,label,chunked_phrase[0]))
             chunked_phrase = []
             chunked_phrase.append(wlp[0])
             label = wlp[1]
         elif wlp[1] == 'ENDPHRASE':
             chunked_phrase.append(wlp[0])
-            phrase_list.append({label:chunked_phrase})
+            phrase_list.append((s_number,label,chunked_phrase))
             label = 'none'
             chunked_phrase = []
         elif wlp[1] == 'none' and label != 'none':
             chunked_phrase.append(wlp[0])
     return phrase_list
 
+secondary_labels = ['DURATION', 'SECTOOL', 'LOCATION', 'TIME', 'TOOL', 'DESCRIPTION', 'ACTOR', 'DEFINITION']
 
-label_set = list(set([wlp[1] for s in sentence_label_set for wlp in s]))
-print(label_set)
+def split_chunks(chunk_list):
+    chunk_set_to_publish = []
+    chunk_to_publish = []
+    i = 0
+    for ch in chunk_list:
+        if ch[1] not in secondary_labels and i == 0:
+           i = 1
+        elif ch[1] not in secondary_labels and i != 0:
+            chunk_set_to_publish.append(chunk_to_publish)
+            chunk_to_publish = []
+        chunk_to_publish.append(ch)
+    chunk_set_to_publish.append(chunk_to_publish)
+    return chunk_set_to_publish
 
+def index_non_secondary_chunks(chunk_list):
+    for i, ch in enumerate(chunk_list):
+        if ch[1] not in secondary_labels:
+           return i
+    return None
 
-secondary_labels = ['DESCRIPTION','BORDER','ACTOR', 'DEFINITION','LOCATION','TOOL']
-ignore_labels = ['BORDER']
+sentences_objects = list(collection.find())
+# print(sentences_objects[3])
+
+sentence_label_set= [(s['sentNumber'],s['sentWords'])for s in sentences_objects]
+# print(sentence_label_set[32])
+
+sentences_split = sent_split_by_border_tag(sentence_label_set)
+# print(sentences_split[32])
+
+# label_set = list(set([wlp[1] for s in sentence_label_set for wlp in s]))
+# print(label_set)
 
 # print(chunk_reconstruction(sentences_split[17]))
 chunk_set = [chunk_reconstruction(s) for s in sentences_split if chunk_reconstruction(s)!= [] ]
-print(chunk_set[27])
 
 
-chunk_set_to_publish = []
-chunk_to_publish = []
-# label = 'none'
-for ch in chunk_set[27]:
-    for k, v in ch.items():
-        if k not in secondary_labels and chunk_set_to_publish != []:
-            chunk_to_publish.append(ch)
-       
-        chunk_to_publish.append(ch)
+chunk_set_split = [split_chunks(s) for s in chunk_set ]
+# print(chunk_set_split[33])
+
+for chs in chunk_set_split:
+    for ch in chs:
+        index = index_non_secondary_chunks(ch)
+        if index is not None:
+            ch.insert(0,ch.pop(index))
+
+
+
+with open('H:/nlp_crap/Articles/extracted.txt', 'w', encoding='utf-8') as f:
+    for s in chunk_set_split:
+        for chs in s:
+            line = ''
+            for i, ch in enumerate(chs):
+                if i == 0:
+                    line = str(ch[0])
+                line = line+ '  ' + ch[1]
+                if isinstance(ch[2], list):
+                    line = line+ '  ' + ' '.join(ch[2])
+                else:
+                    line = line +'  '+ch[2]
+            # print(line)
+            f.write(line)
+            f.write('\n')
+            f.write('\n')
+      # print(chs)
+
 
 
 
